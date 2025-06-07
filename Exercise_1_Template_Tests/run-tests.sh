8f46b2ed8bfe4ff226640af8e7e2a6cc07cf9156
ptudorr <pop.tudorrr@gmail.com>
2025-06-07 17:19:51 +0300

    Test

--- Exercise_1_Template_Tests/run-tests.sh
+++ Exercise_1_Template_Tests/run-tests.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-echo "[INFO] Copying student code..."
-cp -r ../student/src/main/java/* src/main/java/
+echo "[INFO] Copying student code from src/exercises to test project..."
+mkdir -p src/main/java
+cp -r ../student/src/exercises/* src/main/java/
 
 echo "[INFO] Running tests..."
 mvn clean test
