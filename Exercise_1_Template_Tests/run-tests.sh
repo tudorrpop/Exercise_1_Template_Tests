b2e02fc42f91143b34dff7c85febef8a800ced03
ptudorr <pop.tudorrr@gmail.com>
2025-07-07 19:38:28 +0300

    update

--- Exercise_1_Template_Tests/run-tests.sh
+++ Exercise_1_Template_Tests/run-tests.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "[INFO] Copying student code from student repo to test project..."
 mkdir -p src/main/java
-cp -r ../../../../student/Exercise_1_Template/src/* src/main/java/
+cp -r ../../student/Exercise_1_Template/src/* src/main/java/
 
 echo "[INFO] Running tests..."
 mvn clean test
