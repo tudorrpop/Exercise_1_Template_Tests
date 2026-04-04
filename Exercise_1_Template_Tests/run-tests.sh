c4c72549830a65006ac0ff14fb5fbe2c6d33004d
ptudorr <pop.tudorrr@gmail.com>
2026-04-04 14:19:25 +0300

    Correct repository structure

--- Exercise_1_Template_Tests/run-tests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "[INFO] Copying student code from student repo to test project..."
-mkdir -p src/main/java
-cp -r ../../student/Exercise_1_Template/src/* src/main/java/
-
-echo "[INFO] Running tests..."
-mvn clean test -Dmaven.test.failure.ignore=true
